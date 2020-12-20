e1c9358cff379bf852108d2ac3bc4055fac66188
jackmt9 <jackmt9@gmail.com>
2020-12-20 13:26:44 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def collect_multiples(n)
+    multiples = []
+    counter = 3
+    while counter < n do
+        if counter % 3 == 0 || counter % 5 == 0
+            multiples.push(counter)
+        end
+        counter += 1
+    end
+    return multiples
+end
+
+def sum_multiples(n)
+    return collect_multiples(n).reduce(0) { |sum, num| sum + num }
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+    def initialize(n)
+        @n = n
+    end
+
+    def collect_multiples
+        multiples = []
+        counter = 3
+        while counter < @n do
+            if counter % 3 == 0 || counter % 5 == 0
+                multiples.push(counter)
+            end
+            counter += 1
+        end
+        return multiples
+    end
+    
+    def sum_multiples
+        return collect_multiples.reduce(0) { |sum, num| sum + num }
+    end
+
+end
